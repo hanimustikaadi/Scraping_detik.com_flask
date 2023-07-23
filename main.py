2034c8702b636336974e59c5abcc8a6349716faa
hani <mustikahani@yahoo.com>
2023-07-23 18:55:44 +0700

    image

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://www.detik.com/terpopuler')
+#print(res.status_code)
+soup = BeautifulSoup(res.text, 'html.parser')
+populer_area = soup.find(attrs={'class': 'grid-row list-content'})
+title = populer_area.findAll(attrs={'class': 'media__title'})
+images = populer_area.findAll(attrs={'class': 'media__image'})
+
+for image in images:
+    print(image)
